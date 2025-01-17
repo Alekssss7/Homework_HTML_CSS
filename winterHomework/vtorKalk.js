01009014c98ae54585a51b23738726389188a2e6
Alekssss7 <lawleyy7@gmail.com>
2025-01-17 02:28:19 +0100

    Add files via upload

---
+++ winterHomework/vtorKalk.js
@@ -0,0 +1,13 @@
+
+function showNum(input){
+    result.value += input
+}
+
+function clear(){
+    result.value = ""
+}
+//ne znam kade gresham, im gi smeniv i mestata na clear i result i pak funkcijata ne se izvrshuva
+
+function calculate(){
+}
+//shto namesto eval? :(
